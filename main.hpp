390bd3dfc322bfa62c64f80c0076ac3529328a62
msicaja <156959957+msicaja@users.noreply.github.com>
2024-03-15 22:03:45 +0000

    First Run

--- main.hpp
+++ main.hpp
@@ -2,7 +2,39 @@
 //********************
 // Declare Function Prototypes
 //********************
+#include <iostream>
+using namespace std;
+int getNextPrime(int);
+int getPrevPrime(int);
+void getTwoValues(int &, int &);
+
 
 // ******************************
 // Implement all your functions here
 // ******************************
+void getTwoValues(int &x, int &y) {
+    cin >> x >> y;
+}
+
+int getNextPrime(int i) {
+    int j, prime;
+
+    while (1) {
+        for (j = 2; j < i; ++j) {
+            if (i % j == 0) {
+                break;
+            }
+        }
+        if (i == j) {
+            prime = i;
+            break;
+        }
+        ++i;
+    }
+
+    return prime;
+}
+
+int getPrevPrime(int num) {
+    return 12;
+}
